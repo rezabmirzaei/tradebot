6f9d9a620c66e17baeb3e3533384ad1caa38d5ca
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-03-02 13:55:20 +0100

    Configuration

--- tradebot.py
+++ tradebot.py
@@ -1,13 +1,17 @@
+from configparser import ConfigParser
+
 import alpaca_trade_api as tradeapi
 
 from account.session_handler import RestApi
 
-# TODO: Setup proper config handling
-# Authentication and connection details
-api_key = '...'
-api_secret = '...'
-base_url = 'https://paper-api.alpaca.markets'
+# TODO Handle this elsewhere...
+config = ConfigParser()
+# TODO Configure for different environments (e.g. test/paper vs prod)
+config.read('config/paper_account_config.ini')
+api_key = config.get('API', 'key')
+api_secret = config.get('API', 'secret')
+base_url = config.get('API', 'base_url')
 
 restApi = RestApi(api_key, api_secret, base_url)
-
 print(restApi.trading_api().get_account())
+
