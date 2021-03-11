fbb79d2e08514672f13d646af1af609564627031
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-03-11 09:42:18 +0100

    Rename vars

--- autotrade/account_manager.py
+++ autotrade/account_manager.py
@@ -39,9 +39,9 @@ class AccountManager:
         edge = 0
         if probability <= 1.5:
             edge = 1
-        payout_factor = round(probability - 1, 2)
-        percentage_win = math.ceil(1 / probability * 100) + edge
-        percentage_loss = 100 - percentage_win
-        win_factor = percentage_win / 100
+        profit_factor = round(probability - 1, 2)
+        percentage_gain = math.ceil(1 / probability * 100) + edge
+        percentage_loss = 100 - percentage_gain
+        win_factor = percentage_gain / 100
         loss_factor = percentage_loss / 100
-        return round((payout_factor * win_factor - loss_factor) / payout_factor, 3)
+        return round((profit_factor * win_factor - loss_factor) / profit_factor, 3)
